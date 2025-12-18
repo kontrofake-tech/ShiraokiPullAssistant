81b32208b79762ca3d25aae4f4aed45d1a989caf
kontrofake-tech <kontrofake@gmail.com>
2025-12-18 14:05:51 +0100

    whatchya lookin for

--- app.py
+++ app.py
@@ -413,7 +413,7 @@ with col_ui:
     with st.expander("Write a prayer beforehand (Optional)"):
         # We use keys so their values are accessible in the button logic above
         st.text_input("Name (Optional)", placeholder="Trainer", key="p_name")
-        st.text_input("Item of Desire (Optional)", placeholder="SSR Card Name", key="p_item")
+        st.text_input("Item of Desire (Optional)", placeholder="Kitasan Black SSR", key="p_item")
         st.text_area("Prayer Text", placeholder="Shiraoki-sama, please give me the card I want...", height=100, key="prayer_text")
         st.file_uploader("Upload a catalyst (Image, max 10MB)", type=['png', 'jpg', 'jpeg'], label_visibility="collapsed", key="catalyst")
 
